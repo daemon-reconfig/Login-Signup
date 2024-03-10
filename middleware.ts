e8e2eda8489bd7baa245ba97633ac1990bf0d369
daemon-reconfig <mehulishi27@gmail.com>
2024-03-10 09:48:25 +0530

    routing and middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,31 @@
 
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
-
+import { apiAuth, DEFAULT_LOGIN_REDIRECT, publicRoutes, authRoutes } from "./routes";
 const { auth } = NextAuth(authConfig);
 export default auth((req) => {
-  const isLoggedIn = !!req.auth;
+    const { nextUrl } = req;
+    const isLogged = !!req.auth;
+    const isApiRoute = nextUrl.pathname.startsWith(apiAuth);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
+    if(isApiRoute){
+        return null;
+    }
+
+    if(isAuthRoute){
+        if(isLogged){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+
+    if(!isPublicRoute && !isLogged){
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+    
+    return null;
 })
 
 // Optionally, don't invoke Middleware on some paths
