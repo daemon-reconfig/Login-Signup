ba969d1d338081d5f8ac1406742953f8a62677c7
daemon-reconfig <mehulishi27@gmail.com>
2024-03-11 11:18:08 +0530

    trying to fix deploy

--- app/auth/new-password/dist/page.js
+++ app/auth/new-password/dist/page.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var NewPasswordForm_1 = require("../../../components/auth/NewPasswordForm");
+var NewPasswordForm_1 = require("@/components/auth/NewPasswordForm");
 var NewPasswordPage = function () {
     return (React.createElement(NewPasswordForm_1.NewPasswordForm, null));
 };
